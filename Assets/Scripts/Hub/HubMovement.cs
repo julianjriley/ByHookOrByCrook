086b216869f20fb5c86d89eaa51eafd158237d6e
Andrew Kohler <awk480@gmail.com>
2024-09-16 09:53:41 -0400

    Added hub scene, input manager package

---
+++ Assets/Scripts/Hub/HubMovement.cs
@@ -0,0 +1,23 @@
+/*
+ * Script for player movement in the hub. 
+ * Up-down-left-right, classic top-down movement.
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HubMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
