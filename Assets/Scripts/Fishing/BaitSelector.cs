36dd75f43ed1a71c06b0246e1503dbc7d9b3a73e
Jane Nicole Apostol <89412671+jnapostol@users.noreply.github.com>
2024-09-26 00:17:45 -0400

    Started BaitSelection Logic + New Scene

---
+++ Assets/Scripts/Fishing/BaitSelector.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaitSelector : MonoBehaviour
+{
+    // Will mimic LoadoutSelection script. Jane will do tmr bc it is LATE (by tmr i mean 09-26 3pmish)
+
+    // Barrel buttons
+    //  onclick: adds bait type to selection.
+}
