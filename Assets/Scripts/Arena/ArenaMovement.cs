1dc6f5de2c27b78d84092abc226427d80176d6df
brandonDavis264 <73512985+brandonDavis264@users.noreply.github.com>
2024-09-29 02:14:31 -0400

    Bear Move 2.0

--- Assets/Scripts/Arena/ArenaMovement.cs
+++ Assets/Scripts/Arena/ArenaMovement.cs
@@ -1,17 +1,35 @@
 using System.Collections;
+using UnityEditor.ShaderGraph;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class ArenaMovement : MonoBehaviour
 {
-    public float speed = 5f;
-    public float jumpForce = 7f;
-    
+    private ActionControls controls;
+
+    //X-Movement
+    public float MovementSpeed = 5f;
+
+    //Jumping
+    public float jumpHeight = 7f;
+    public int maxNumberOfJumps = 1;
+    private bool canJump;
+    private int remainingJumps;
+    [Tooltip("Time Player Has to jump when Leaving the edge of a platform")]
+    public float coyoteTime = 0.2f;
+    private float coyoteTimeCounter;
+    private bool isGrounded;
+    [Tooltip("Jump Buffer: The time window for players to jump right before they land on the ground")]
+    public float jumpBufferTime = 0.2f;
+    private float jumpBufferCounter;
+
+
+
     //Dash Vars
     private bool canDash = true;
     private bool isDashing;
-    public float dashingPower = 24f;
-    public float dashTime = 0.2f;
+    public float dashSpeed = 24f;
+    public float dashDistance = 0.2f;
     public float dashingCooldown = 0.5f;
     //Dash With and Without Gravity can be toggled for feel
     public bool ToggleGravityWhenDashing = false;
@@ -25,43 +43,109 @@ public class ArenaMovement : MonoBehaviour
     private float horizontal;
 
     private Rigidbody rb;
-    private bool isGrounded;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         _anim = GetComponent<Animator>();
         _sr = GetComponent<SpriteRenderer>();
+
+        remainingJumps = maxNumberOfJumps;
+
+        controls.Player.JumpAction.Enable();
+        controls.Player.JumpAction.started += Jump;
+        controls.Player.JumpAction.performed += Jump;
+        controls.Player.JumpAction.canceled += Jump;
+
+        controls.Player.MoveArena.Enable();
+        controls.Player.MoveArena.started += Move;
+        controls.Player.MoveArena.canceled += Move;
+
+
+        controls.Player.Dash.Enable();
+        controls.Player.Dash.started += Dash;
+        //controls.Player.Dash.canceled += Move;
     }
 
+    private void Awake()
+    {
+        controls = new ActionControls();
+    }
+
+    //Update Will be Removed after combat scrip is finished (Only Here right for debugging Purposes)
     void Update()
     {
         if (isDashing)
             return;
-        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+        //Check Comment Above Function Header
         AnimatePlayer2D();
+    }
+    
+    void FixedUpdate()
+    {
+        if (isGrounded)
+        {
+            coyoteTimeCounter = coyoteTime;
+        }
+        else
+            coyoteTimeCounter -= Time.deltaTime;
+
+
+
+        if (isDashing)
+            return;
+
+        //Jump Buffer Logic
+        if ((jumpBufferCounter > 0f && coyoteTimeCounter > 0f) && canJump)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+            jumpBufferCounter = 0f;
+        }
+
+        if (isGrounded)
+        {
+            remainingJumps = maxNumberOfJumps;
+        }
+
+        rb.velocity = new Vector2(horizontal * MovementSpeed, rb.velocity.y);
+
+        jumpBufferCounter -= Time.deltaTime;
     }
 
     public void Jump(InputAction.CallbackContext context)
     {
-        if (context.performed && isGrounded)
+
+        if (context.started)
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpBufferCounter = jumpBufferTime;
+            canJump = true;
+            if (remainingJumps > 1  && !isGrounded)
+            {
+                print("Remaining Jumps " + remainingJumps);
+                rb.velocity = new Vector2(rb.velocity.x, jumpHeight); 
+                remainingJumps--;
+            }
+
         }
 
-        if (context.canceled && rb.velocity.y > 0f)
+        
+        if (context.canceled && rb.velocity.y > 0)
         {
+            canJump = false;
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+            coyoteTimeCounter = 0f;
         }
+       
     }
     public void Move(InputAction.CallbackContext context)
     {
         horizontal = context.ReadValue<Vector2>().x;
-    }
 
+    }
+          
     public void Dash(InputAction.CallbackContext context)
     {
-        if (canDash && context.performed)
+        if (canDash && context.started)
         {
             StartCoroutine(Dasher(rb.velocity.x));
         }
@@ -76,22 +160,21 @@ public class ArenaMovement : MonoBehaviour
         isGrounded = false;
     }
 
+    private void OnCollisionStay(Collision collision)
+    {
+        isGrounded = true;
+    }
+
+    //Dashes Based off the Orientation of the Sprite
     private IEnumerator Dasher(float direction)
     {
         canDash = false;
         isDashing = true;
-        
         int leftOrRight = IsFacingRight > 0 ? 1 : -1;
         rb.useGravity = !ToggleGravityWhenDashing;
 
         _anim.SetBool("IsMoving", true);
-        /*if(direction == 0 && !IsFacingRight)
-        {
-            _sr.flipX = true;
-            IsFacingRight = true;
-        }*/
-        rb.velocity = new Vector2(transform.localScale.x * dashingPower * leftOrRight, 0f);
-        yield return new WaitForSeconds(dashTime);
+        yield return new WaitForSeconds(dashDistance);
         _anim.SetBool("IsMoving", false);
         isDashing = false;
 
@@ -100,6 +183,16 @@ public class ArenaMovement : MonoBehaviour
         canDash = true;
     }
 
+
+    /*private IEnumerator Mover()
+    {
+        
+    }*/
+    /* Player Animation Function if ever Needed Can Be Commented out after Pull request
+            - I'm only keeping it in my code right now because dash Coroutine needs the orientation of
+                of the Sprite to know which way to dash, since the combat script handles that then I can 
+                take out the animation after its pushed so I can still test my code
+    */
     private void AnimatePlayer2D()
     {
         if ((horizontal != 0 || horizontal != 0) && !IsIdle)
