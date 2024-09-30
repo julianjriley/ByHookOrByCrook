525a1cf4cc0ce5fb5eae1f7b350364cb65258568
andres-maldonado <155512647+andres-maldonado@users.noreply.github.com>
2024-09-30 00:00:35 -0400

    FMOD Integration and Placeholder Sounds
    Got FMOD For Unity set up and put in a few Events to test it out.

--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -1,13 +1,54 @@
+using FMOD.Studio;
+using FMODUnity;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+    private EventInstance ambienceEventInstance;
+    private EventInstance musicEventInstance;
+
+    private List<EventInstance> eventInstances;
+
+    public static SoundManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError("Found more than one Audio Manager in the scene.");
+        }
+        Instance = this;
+
+        eventInstances = new List<EventInstance>();
+        
+    }
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         
+    }
+    public EventInstance CreateInstance(EventReference eventReference)
+    {
+        EventInstance eventInstance = RuntimeManager.CreateInstance(eventReference);
+        eventInstances.Add(eventInstance);
+        return eventInstance;
+    }
+    public void PlayOneShot(EventReference sound, Vector3 worldPos)
+    {
+        RuntimeManager.PlayOneShot(sound, worldPos);
+    }
+    public void InitializeMusic(EventReference musicEventReference)
+    {
+        musicEventInstance = CreateInstance(musicEventReference);
+        musicEventInstance.start();
+    }
+    private void InitializeAmbience(EventReference ambienceEventReference)
+    {
+        ambienceEventInstance = CreateInstance(ambienceEventReference);
+        ambienceEventInstance.start();
     }
 
     // Update is called once per frame
