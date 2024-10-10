6cf3a987fe4a70643b3c6f39058b1a8d75c51c11
ChrisTruta <christophertruta@gmail.com>
2024-10-10 16:29:36 -0400

    Boring Fishing Line Ready

---
+++ Assets/Scripts/Fishing/FishingLineController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishingLineController : MonoBehaviour
+{
+    private LineRenderer _lineRenderer;
+
+    [SerializeField]
+    private Transform[] _linePoints;
+
+    void Start()
+    {
+        _lineRenderer = GetComponent<LineRenderer>();
+        _lineRenderer.SetPosition(0, _linePoints[0].position);
+    }
+
+    void Update()
+    {
+        // for our current purposes, our fishing line never moves origins, so I don't need to update the point of the origin.
+        // IF THIS CHANGES, simply create an iterative loop that updates all _linePoints in the Transform array
+        _lineRenderer.SetPosition(1, _linePoints[1].position);
+        // _linePoints.
+    }
+}
