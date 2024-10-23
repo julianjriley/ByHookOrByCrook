220a9693753a2f00153ac17cb5fe14440f8ed817
Jane Nicole Apostol <89412671+jnapostol@users.noreply.github.com>
2024-10-22 22:53:24 -0400

    Crosshair Colors + Size + Opacity
    Crosshair can now via r g b, size, and opacity (forgot to change color tmp text to say opacity).
    Bug: When resizing the crosshair, it will not go back to its default start size. Solution is to change slider Min Value.

--- Assets/Scripts/Options/CustomCrosshair.cs
+++ Assets/Scripts/Options/CustomCrosshair.cs
@@ -1,38 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class CustomCrosshair : MonoBehaviour
 {
 
-    private SpriteRenderer _renderer;
-    public Sprite DefaultCrosshair;
+    // private SpriteRenderer _renderer;
+    public Image DefaultCrosshair;
 
-    public Slider Red, Green, Blue, Size;
+    public Slider Red, Green, Blue, Opacity, Size;
+    public TextMeshProUGUI RPercent, GPercent, BPercent, OPercent;
 
     private void Start()
     {
-        Cursor.visible = false;
-        _renderer = GetComponent<SpriteRenderer>();
-        Cursor.SetCursor(DefaultCrosshair.texture,new Vector2(0,0), CursorMode.ForceSoftware);
+        //Cursor.visible = false;
+        DefaultCrosshair = GetComponent<Image>();
+       // _renderer = GetComponent<SpriteRenderer>();
+        
+
+        //Cursor.SetCursor(DefaultCrosshair.texture,new Vector2(0,0), CursorMode.ForceSoftware);
     }
 
     private void Update()
     {
-        Vector2 _cursorPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 _cursorPosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
         GameManager.Instance.GamePersistent.Crosshair = this;
-        GameManager.Instance.GamePersistent.crosshairSprite = DefaultCrosshair;
+        GameManager.Instance.GamePersistent.crosshairSprite = DefaultCrosshair.sprite;
     }
 
     public void ChangeColor()
     {
-        _renderer.color = new Color(Red.value, Green.value, Blue.value);
+        DefaultCrosshair.color = new Color(Red.value, Green.value, Blue.value, Opacity.value);
+
+        // Change percents
+
+        RPercent.text = ((int)Red.value * 100).ToString() + " %";
+        GPercent.text = ((int)Green.value * 100).ToString() + " %";
+        BPercent.text = ((int)Blue.value * 100).ToString() + " %";
+        OPercent.text = ((int)Opacity.value * 100).ToString() + " %";
+
+
     }
     public void ChangeSize()
     {
-        //DefaultCrosshair.textureRect.size = 
+        float size = Size.value;
+        DefaultCrosshair.gameObject.GetComponent<RectTransform>().localScale = new Vector2(size, size);
     }
 }
