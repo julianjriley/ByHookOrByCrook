f162f2b43ee9fc3d966dd6f44dd22b982e8eb2d0
Julian Riley <87546620+julianjriley@users.noreply.github.com>
2024-11-26 22:46:44 -0500

    Added script for blobfish

---
+++ Assets/Scripts/Inventory/WeaponScripts/Blobfish.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blobfish : WeaponInstance
+{
+    public override void Fire(Vector3 direction)
+    {
+        base.Fire(direction);
+    }
+}
