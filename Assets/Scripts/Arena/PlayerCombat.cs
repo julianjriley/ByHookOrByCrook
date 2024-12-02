931e5841f63654666ec601002907a9995cf67953
Jodi Foster <fosterjv111@gmail.com>
2024-12-02 11:03:24 -0500

    deleted a weird import of unknown origin

--- Assets/Scripts/Arena/PlayerCombat.cs
+++ Assets/Scripts/Arena/PlayerCombat.cs
@@ -2,7 +2,6 @@ using FMODUnity;
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
