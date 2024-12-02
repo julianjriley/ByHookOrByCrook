6439b6d95afbcc353db5a6cd560ce81185fb1174
Julian Riley <87546620+julianjriley@users.noreply.github.com>
2024-12-02 10:09:03 -0500

    Revert "Merge branch 'JulianR_OverheatUIPolish' into JulianR_CrossHairOverheat"
    This reverts commit 3e608a15f93a84867ac14086106e1e18961d0ac0, reversing
    changes made to 21da0a2f553390c6b49fc5a6b22e125ec78722bd.

--- Assets/Scripts/Arena/GunSlot.cs
+++ Assets/Scripts/Arena/GunSlot.cs
@@ -7,33 +7,12 @@ public class GunSlot : MonoBehaviour
 {
     [SerializeField] Image _gunImage;
     [SerializeField] RectTransform _overheat;
-    [SerializeField] Image _overheatBarImage;
     WeaponInstance _weapon;
 
-    [SerializeField] Color _heatingUpColor;
-    [SerializeField] Color _overheatedColor;
-    [SerializeField] GameObject _overheatedImage;
     private void Update()
     {
         if (_weapon != null)
-        {
             _overheat.sizeDelta = new Vector2(100, _weapon.GetHeatLevel());
-            if(_weapon.GetOverHeatedState())
-            {
-                _overheatBarImage.color = _overheatedColor;
-                if(!_overheatedImage.activeSelf)
-                    _overheatedImage.SetActive(true);
-            }
-            else
-            {
-                _overheatBarImage.color = _heatingUpColor;
-                if (_overheatedImage.activeSelf)
-                    _overheatedImage.SetActive(false);
-            }
-
-        }
-            
-
     }
 
     public void AssignWeapon(WeaponInstance weapon)
