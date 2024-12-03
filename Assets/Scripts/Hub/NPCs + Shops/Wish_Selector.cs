899d3a35a2f84df971bf27b7b2f888520cf302f7
Andrew Kohler <awk480@gmail.com>
2024-12-03 11:17:21 -0500

    Added a frick ton of fortunes

--- Assets/Scripts/Hub/NPCs + Shops/Wish_Selector.cs
+++ Assets/Scripts/Hub/NPCs + Shops/Wish_Selector.cs
@@ -17,17 +17,17 @@ public class Wish_Selector : MonoBehaviour
         _npc = GetComponent<NPCInteractor>();
         // Lis of convos  = book.general
         List<Conversation> fortunes = wishBook.generalConvos;
-        Debug.Log("Fortunes Size " + fortunes.Count);
+        //Debug.Log("Fortunes Size " + fortunes.Count);
         if (fortunes.Count > 0)
         {
             //Generate a random number from 0 to the length of the list
             int generatedIndex = Random.Range(0, fortunes.Count);
             // npc.set convo ()
-            Debug.Log("Selected " + generatedIndex);
+           // Debug.Log("Selected " + generatedIndex);
             _npc.SetConversation(fortunes[generatedIndex], 0);
         }
-        else
-            Debug.Log("There are No Fortunes when there should be");
+        //else
+            //Debug.Log("There are No Fortunes when there should be");
     }
 
 }
