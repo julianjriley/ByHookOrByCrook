e948a8aeb610c36a612db343eb0204e5ca1c7c53
brandonDavis264 <73512985+brandonDavis264@users.noreply.github.com>
2024-12-05 00:59:57 -0500

    Skipper

--- Assets/Scripts/Inventory/WeaponScripts/SkipperGlock.cs
+++ Assets/Scripts/Inventory/WeaponScripts/SkipperGlock.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class SkipperGlock : WeaponInstance
 {
     [SerializeField] GameObject _projectile;
+    [SerializeField] Animator _anim;
     public override void Fire(Vector3 direction)
     {
+        _anim = gameObject.GetComponent<Animator>();
+
         for (int i = 0; i < _weapon.ProjectileCount; i++)
         {
             GameObject projectile = Instantiate(_projectile, _firePoint.position, Quaternion.FromToRotation(Vector3.up, _direction));
@@ -15,6 +18,7 @@ public class SkipperGlock : WeaponInstance
             SkipperProjectile skipperProjectile = projectile.GetComponent<SkipperProjectile>();
             skipperProjectile.AssignStats(_weapon);
             _heatLevel += _weapon.HeatBuildup;
+            _anim.Play("Glock", -1, 0);
         }
         StartCoroutine(FireRate());
         _autoFireCoroutine = StartCoroutine(FireAuto(_direction));
